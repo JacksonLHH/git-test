a530f3b2bc0fc9f01ebb1e8a4bc10ccabbf2257b
Jackson_Wang <https://github.com/JacksonLHH/git_test.git>
2022-01-05 21:53:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * create by： Jackson
+ *
+ * @Data： 2022/1/5 0005 21:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
